3678cacaf5126d4086bf91c49e6efc2f73a331db
SystemGlitch <jeremy.la@outlook.fr>
2020-01-23 14:21:10 +0100

    Init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module ci-api-go-4al
+
+go 1.13
+
+require (
+	github.com/System-Glitch/goyave/v2 v2.4.3
+	github.com/bxcodec/faker/v3 v3.2.0
+	github.com/jinzhu/gorm v1.9.12
+)
---
+++ http/route/route.go
@@ -0,0 +1,33 @@
+package route
+
+import (
+	"ci-api-go-4al/http/controller/hello"
+	"ci-api-go-4al/http/request/echorequest"
+
+	"github.com/System-Glitch/goyave/v2"
+	"github.com/System-Glitch/goyave/v2/cors"
+)
+
+// Routing is an essential part of any Goyave application.
+// Routes definition is the action of associating a URI, sometimes having
+// parameters, with a handler which will process the request and respond to it.
+
+// Routes are defined in routes registrer functions.
+// The main route registrer is passed to "goyave.Start()" and is executed
+// automatically with a newly created root-level router.
+
+// Register all the application routes. This is the main route registrer.
+func Register(router *goyave.Router) {
+
+	// Applying default CORS settings (allow all methods and all origins)
+	// Learn more about CORS options here: https://system-glitch.github.io/goyave/guide/advanced/cors.html
+	router.CORS(cors.Default())
+
+	// Register your routes here
+
+	// Route without validation
+	router.Route("GET", "/hello", hello.SayHi, nil)
+
+	// Route with validation
+	router.Route("GET", "/echo", hello.Echo, echorequest.Echo)
+}
