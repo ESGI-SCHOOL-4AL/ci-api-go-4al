093e3750df6c412bc4dd97dfb2092b6ba3d525c1
LAURENT Louis <louis_laurent@aol.fr>
2020-01-30 14:32:34 +0100

    feat(tests): add some tests who don't work for the api

--- test/text_test.go
+++ test/text_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/System-Glitch/goyave/v2"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 type TextTestSuite struct { // Create a test suite for the Hello controller
@@ -17,7 +18,8 @@ func (suite *TextTestSuite) SetupTest() {
 	seeder.Text()
 }
 
-func (suite *TextTestSuite) TestHello() {
+// Count number of result and check if not nil
+func (suite *TextTestSuite) TestGetAll() {
 	suite.RunServer(route.Register, func() {
 		resp, err := suite.Get("/text", nil)
 		suite.Nil(err)
@@ -26,15 +28,75 @@ func (suite *TextTestSuite) TestHello() {
 			err := suite.GetJSONBody(resp, &json)
 			suite.Nil(err)
 			if err == nil {
-				// suite.Equal("value", json["title"])
-				// suite.Equal("test", json["content"])
 				suite.Equal(json["title"], "test")
 			}
 		}
 	})
-
 }
 
+// Count and check if content not nil
+func (suite *TextTestSuite) TestGetOne() {
+	suite.RunServer(route.Register, func() {
+		resp, err := suite.Get("/text/1", nil)
+		suite.Nil(err)
+		if err == nil {
+			json := map[string]interface{}{}
+			err := suite.GetJSONBody(resp, &json)
+			suite.Nil(err)
+			if err == nil {
+				suite.Equal("test", json["title"])
+			}
+		}
+	})
+}
+
+// Check error code
+func (suite *TextTestSuite) TestGetUnexistedOne() {
+	suite.RunServer(route.Register, func() {
+		resp, err := suite.Get("/text/666", nil)
+		suite.Nil(err)
+		if err == nil {
+			json := map[string]interface{}{}
+			err := suite.GetJSONBody(resp, &json)
+			suite.Nil(err)
+			if err == nil {
+				suite.Equal("test", json["title"])
+			}
+		}
+	})
+}
+
+// Check if the target is deleted
+func (suite *TextTestSuite) TestDeleteOne() {
+	suite.RunServer(route.Register, func() {
+		resp, err := suite.Get("/text/1", nil)
+		suite.Nil(err)
+		if err == nil {
+			json := map[string]interface{}{}
+			err := suite.GetJSONBody(resp, &json)
+			suite.Nil(err)
+			if err == nil {
+				suite.Equal("test", json["title"])
+			}
+		}
+	})
+}
+
+// Check if data evolve
+func (suite *TextTestSuite) TestUpdateOne() {
+	suite.RunServer(route.Register, func() {
+		resp, err := suite.Get("/text/1", nil)
+		suite.Nil(err)
+		if err == nil {
+			json := map[string]interface{}{}
+			err := suite.GetJSONBody(resp, &json)
+			suite.Nil(err)
+			if err == nil {
+				suite.Equal("test", json["title"])
+			}
+		}
+	})
+}
 func TestTextSuite(t *testing.T) { // Run the test suite
 	goyave.RunTest(t, new(TextTestSuite))
 }
